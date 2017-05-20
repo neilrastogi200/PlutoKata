61fc00428201b13cc63c4f5cbb87ce5bea3dcb74
neil.rastogi <neil.rastogi@tungsten-network.com>
2017-05-20 20:24:18 +0100

    Add project files.

---
+++ RoverKata/Rover.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverKata
+{
+    public class Rover
+    {
+        public int CoordinateX { get; private set; }
+        public int CoordinateY { get; private set; }
+
+        public string Direction { get; private set;}
+
+        private readonly int _gridSize;
+
+        public Rover(int gridSize)
+        {
+            CoordinateX = 0;
+            CoordinateY = 0;
+            _gridSize = gridSize;
+
+
+        }
+
+
+
+        public void Move(int coordinateX, int coordinateY, string direction, string command)
+        {
+            if (command == "F" && direction == "N")
+            {
+                Direction = direction;
+                IncreasePositionY();
+            }
+        }
+
+        private void IncreasePositionY()
+        {
+            if (CoordinateY < _gridSize)
+            {
+                CoordinateY++;
+            }
+        }
+    }
+}
---
+++ RoverKataTests/RoverTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RoverKata;
+
+namespace RoverKataTests
+{
+    [TestFixture]
+    public class RoverTests
+    {
+        [Test]
+        public void Given_Position_With_Default_Direction_And_Forward_Command_Returns_0_1_N()
+        {
+          
+            var rover = new Rover(100);
+
+            rover.Move(0, 0, "N", "F");
+
+            Assert.That(rover.CoordinateX, Is.EqualTo(0));
+            Assert.That(rover.CoordinateY, Is.EqualTo(1));
+            Assert.That(rover.Direction, Is.EqualTo("N"));
+            
+
+        }
+    }
+}
